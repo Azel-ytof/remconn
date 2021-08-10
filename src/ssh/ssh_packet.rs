5da862f9605d59e4f5a0d94859c45bb40b4ed99f
Anthony LAURENT <50252147+Azel-ytof@users.noreply.github.com>
2021-08-10 21:37:12 +0200

    Continue SshPacket impl

--- src/ssh/ssh_packet.rs
+++ src/ssh/ssh_packet.rs
@@ -6,7 +6,7 @@
 use rand::Rng;
 
 pub struct SshPacket {
-    packet_length: u32,
+    packet_length: [u8; 4],
     padding_length: u8,
     payload: String,
     random_padding: Vec<u8>,
@@ -14,24 +14,37 @@ pub struct SshPacket {
 }
 
 impl SshPacket {
-    fn packet_length(&self) {
-        // size of padding_length + payload + random_padding
-        todo!()
-    }
+    pub fn new(payload: String) -> Self {
+        let padding_length = 8;
+        let random_padding = SshPacket::generate_random_padding(padding_length);
+        let payload_length = payload.len() as u8;
+        let packet_length = [0, 0, 0, 0];
+        // self.packet_length = self.padding_length + payload_length + self.padding_length;
 
-    fn payload(&self) -> &[u8] {
-        let expected_length: usize = self.packet_length as usize - self.padding_length as usize - 1;
-        let payload_bytes = self.payload.as_bytes();
-
-        if payload_bytes.len() != expected_length {
-            todo!()
+        Self {
+            packet_length,
+            padding_length,
+            payload,
+            random_padding,
         }
-
-        payload_bytes
     }
 
-    fn generate_random_padding(&self) -> Vec<u8> {
-        let mut a = vec![0; self.padding_length as usize];
+    pub fn into_bytes(&self) -> &[u8] {
+        let mut final_packet: Vec<u8> = Vec::new();
+
+        final_packet.extend_from_slice(&self.packet_length);
+        final_packet.push(self.padding_length);
+        final_packet.extend_from_slice(self.payload.as_bytes());
+        final_packet.extend(self.random_padding.clone());
+        // final_packet.push(self.mac);
+
+        let t = &final_packet[..];
+
+        t
+    }
+
+    fn generate_random_padding(padding_length: u8) -> Vec<u8> {
+        let mut a = vec![0; padding_length as usize];
 
         let mut rng = rand::thread_rng();
         for b in a.iter_mut() {
@@ -39,17 +52,5 @@ impl SshPacket {
         }
 
         a
-    }
-}
-
-impl Default for SshPacket {
-    fn default() -> Self {
-        Self {
-            packet_length: 0,
-            padding_length: 8,
-            payload: String::from(""),
-            random_padding: Vec::new(),
-            // mac: ,
-        }
     }
 }
