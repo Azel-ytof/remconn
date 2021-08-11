4a5f4f32fb89b07909b46d6cf1d494893051fdbb
Anthony LAURENT <50252147+Azel-ytof@users.noreply.github.com>
2021-08-11 22:53:38 +0200

    SshPacket : Change into_bytes to consume instance when called (but always build error to resolve)

--- src/ssh/ssh_packet.rs
+++ src/ssh/ssh_packet.rs
@@ -6,7 +6,7 @@
 use rand::Rng;
 
 pub struct SshPacket {
-    packet_length: [u8; 4],
+    packet_length: u32,
     padding_length: u8,
     payload: String,
     random_padding: Vec<u8>,
@@ -17,9 +17,9 @@ impl SshPacket {
     pub fn new(payload: String) -> Self {
         let padding_length = 8;
         let random_padding = SshPacket::generate_random_padding(padding_length);
-        let payload_length = payload.len() as u8;
-        let packet_length = [0, 0, 0, 0];
-        // self.packet_length = self.padding_length + payload_length + self.padding_length;
+        let payload_length = payload.len() as u32;
+        // 1 is for padding_length byte
+        let packet_length = 1 + payload_length + padding_length as u32;
 
         Self {
             packet_length,
@@ -29,18 +29,16 @@ impl SshPacket {
         }
     }
 
-    pub fn into_bytes(&self) -> &[u8] {
+    pub fn into_bytes(self) -> &'static [u8] {
         let mut final_packet: Vec<u8> = Vec::new();
 
-        final_packet.extend_from_slice(&self.packet_length);
+        final_packet.extend_from_slice(&self.packet_length.to_be_bytes());
         final_packet.push(self.padding_length);
         final_packet.extend_from_slice(self.payload.as_bytes());
         final_packet.extend(self.random_padding.clone());
         // final_packet.push(self.mac);
 
-        let t = &final_packet[..];
-
-        t
+        &final_packet[..]
     }
 
     fn generate_random_padding(padding_length: u8) -> Vec<u8> {
