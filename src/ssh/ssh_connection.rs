6a34dfee8e83f361b2dc4f2f2db41f42bbe140e9
Anthony LAURENT <50252147+Azel-ytof@users.noreply.github.com>
2021-08-15 21:17:51 +0200

    Correct doc en code of SshConnection

--- src/ssh/ssh_connection.rs
+++ src/ssh/ssh_connection.rs
@@ -8,7 +8,7 @@ pub const DEFAULT_TCP_PORT: &str = "22";
 
 /// A `SshConnection` type to manage a connection to a host using ssh protocol
 ///
-/// It is composed by a host name, a tcp port and an user.
+/// You have to connect to a host with a TCP port and an user, and next you could sand messages/commands.
 ///
 /// # Examples
 ///
@@ -17,39 +17,80 @@ pub const DEFAULT_TCP_PORT: &str = "22";
 /// let tcp_port = String::from(remconn::ssh::ssh_connection::DEFAULT_TCP_PORT);
 /// let user = String::from("admin");
 ///
-/// match &mut SshConnection::connect(hostname, tcp_port, user) {
-///     Ok(ssh_connection) => {
-///         ssh_connection.send(String::from("echo test"));
-///         ssh_connection.disconnect();
-///     },
-///     Err(e) => println!("Error : {0}", e)
+/// let mut ssh_connection = SshConnection::new(hostname, tcp_port, user);
+///
+/// if let Err(e) = ssh_connection.connect() {
+///     println!("Error : {0}", e);
+///     return;
 /// }
+///
+/// // You could do a loop
+/// match ssh_connection.send(String::from("echo test")) {
+///     Ok(_) => todo!(),
+///     Err(e) => println!("Error : {0}", e),
+/// }
+///
+/// ssh_connection.disconnect();
 /// ```
+#[derive(Debug)]
 pub struct SshConnection {
     pub hostname: String,
     pub tcp_port: String,
     pub user: String,
-    stream: TcpStream,
+    stream: Option<TcpStream>,
 }
 
 impl SshConnection {
-    /// Create an instance by connecting to given host on given port with given user
+    /// Create an instance
     ///
     /// Return a `SshConnection` instance if suceed, or a [`SshError`] otherwise
     ///
     /// [`SshError`]: SshError
-    pub fn connect(hostname: String, tcp_port: String, user: String) -> Result<Self, SshError> {
-        let host = format!("{0}:{1}", hostname, tcp_port);
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let hostname = String::from(remconn::ssh::ssh_connection::LOCALHOST_HOSTNAME);
+    /// let tcp_port = String::from(remconn::ssh::ssh_connection::DEFAULT_TCP_PORT);
+    /// let user = String::from("user");
+    ///
+    /// let mut ssh_connection = SshConnection::new(hostname, tcp_port, user);
+    /// ```
+    pub fn new(hostname: String, tcp_port: String, user: String) -> Self {
+        Self {
+            hostname,
+            tcp_port,
+            user,
+            stream: None,
+        }
+    }
+
+    /// Connect to the host
+    ///
+    /// Return nothing if succeed, or a [`SshError`] otherwise
+    ///
+    /// [`SshError`]: SshError
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// // ssh_connection already instanciate before
+    /// if let Err(e) = ssh_connection.connect() {
+    ///     println!("Error : {0}", e);
+    /// }
+    /// ```
+    pub fn connect(&mut self) -> Result<(), SshError> {
+        if let Some(_) = self.stream {
+            return Err(SshError::new(String::from("Already connected")));
+        }
+
+        let host = format!("{0}:{1}", self.hostname, self.tcp_port);
 
         match TcpStream::connect(&host) {
             Ok(stream) => {
                 println!("Should connect to {0}", host);
-                Ok(Self {
-                    hostname,
-                    tcp_port,
-                    user,
-                    stream,
-                })
+                self.stream = Some(stream);
+                todo!()
             }
             Err(e) => Err(SshError::new(format!(
                 "Couldn't connect to server : {0}",
@@ -59,19 +100,54 @@ impl SshConnection {
     }
 
     /// Send message (or command) through the SSH connection
-    pub fn send(&mut self, command: String) {
-        let ssh_packet = SshPacket::new(command);
-        let buffer = ssh_packet.into_bytes();
-
-        match self.stream.write(&buffer[..]) {
-            Ok(_) => println!("ok"),
-            Err(e) => println!("err : {0}", e),
+    ///
+    /// Return nothing if succeed, or a [`SshError`] otherwise
+    ///
+    /// [`SshError`]: SshError
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// // ssh_connection already instanciate and been connected before
+    /// if let Err(e) = ssh_connection.send(String::from("echo test")) {
+    ///     println!("Error : {0}", e);
+    /// }
+    /// ```
+    pub fn send(&mut self, command: String) -> Result<(), SshError> {
+        match &mut self.stream {
+            Some(stream) => {
+                let ssh_packet = SshPacket::new(command);
+                let buffer = ssh_packet.into_bytes();
+                match stream.write(&buffer[..]) {
+                    Ok(_) => {
+                        // stream.read(&mut [0; 128]);
+                        todo!()
+                    }
+                    Err(e) => {
+                        return Err(SshError::new(format!(
+                            "An error occured while sending datas : {0}",
+                            e,
+                        )))
+                    }
+                }
+            }
+            None => {
+                return Err(SshError::new(String::from(
+                    "You have to connect to the server before sending commands",
+                )))
+            }
         }
-        // stream.read(&mut [0; 128]);
-        todo!()
     }
 
     /// Disconnect from the SSH connection
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// // ssh_connection already instanciate and been connected before
+    /// ssh_connection.disconnect();
+    /// // Be care, instance is consume next this called
+    /// ```
     pub fn disconnect(self) {
         println!(
             "Should disconnect from {0}:{1}",
