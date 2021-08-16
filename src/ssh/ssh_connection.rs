865195a218a8f2924aefd3ee0f3e5cffe924b96c
Anthony LAURENT <50252147+Azel-ytof@users.noreply.github.com>
2021-08-16 22:42:32 +0200

    Improving SshConnection

--- src/ssh/ssh_connection.rs
+++ src/ssh/ssh_connection.rs
@@ -26,9 +26,12 @@ pub const DEFAULT_TCP_PORT: &str = "22";
 ///     return;
 /// }
 ///
-/// // You could do a loop
-/// match ssh_connection.send(String::from("echo test")) {
-///     Ok(_) => todo!(),
+/// if let Err(e) = ssh_connection.send(String::from("echo test")) {
+///     println!("Error : {0}", e);
+/// }
+///
+/// match ssh_connection.read() {
+///     Ok(out) => println!("Received {0}", out),
 ///     Err(e) => println!("Error : {0}", e),
 /// }
 ///
@@ -140,35 +143,84 @@ impl SshConnection {
     /// }
     /// ```
     pub fn send(&mut self, command: String) -> Result<(), SshError> {
-        let buffer = &mut [0; 128];
-
         match &mut self.stream {
             Some(stream) => {
                 let ssh_packet = SshPacket::new(command);
                 let ssh_packet_bytes = ssh_packet.into_bytes();
 
-                match stream.write(&ssh_packet_bytes[..]) {
-                    Ok(input_length) => match stream.read(buffer) {
-                        Ok(output_length) => todo!(),
-                        Err(e) => {
-                            return Err(SshError::new(format!(
-                                "An error occured while reading datas : {0}",
-                                e,
-                            )))
-                        }
-                    },
-                    Err(e) => {
-                        return Err(SshError::new(format!(
-                            "An error occured while sending datas : {0}",
-                            e,
-                        )))
-                    }
+                if let Err(e) = stream.write(&ssh_packet_bytes[..]) {
+                    return Err(SshError::new(format!(
+                        "An error occured while sending datas : {0}",
+                        e,
+                    )));
                 }
+
+                Ok(())
             }
             None => {
                 return Err(SshError::new(String::from(
                     "You have to connect to the server before sending commands",
                 )))
+            }
+        }
+    }
+
+    /// Read message through the SSH connection
+    ///
+    /// Return the output if succeed, or a [`SshError`] otherwise
+    ///
+    /// [`SshError`]: SshError
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use remconn::ssh::ssh_connection::SshConnection;
+    ///
+    /// let hostname = String::from(remconn::ssh::ssh_connection::LOCALHOST_HOSTNAME);
+    /// let tcp_port = String::from(remconn::ssh::ssh_connection::DEFAULT_TCP_PORT);
+    /// let user = String::from("user");
+    ///
+    /// let mut ssh_connection = SshConnection::new(hostname, tcp_port, user);
+    ///
+    /// // Result to handle
+    /// ssh_connection.connect();
+    ///
+    /// if let Ok(out) = ssh_connection.read() {
+    ///     println!("Message received from the host : {0}", out);
+    /// } else {
+    ///     println!("Couldn't receive message from the host");
+    /// }
+    /// ```
+    pub fn read(&mut self) -> Result<String, SshError> {
+        let buffer = &mut [0; 128];
+
+        match &mut self.stream {
+            Some(stream) => {
+                if let Err(e) = stream.read(buffer) {
+                    return Err(SshError::new(format!(
+                        "An error occured while reading datas : {0}",
+                        e,
+                    )));
+                }
+
+                Ok(self.get_string_from_slice(buffer))
+            }
+            None => {
+                return Err(SshError::new(String::from(
+                    "You have to connect to the server before reading",
+                )))
+            }
+        }
+    }
+
+    /// Convert slice of byte to String
+    /// Non UTF-8 character will be replace by '?'
+    fn get_string_from_slice(&self, slice: &[u8]) -> String {
+        match String::from_utf8(slice.to_vec()) {
+            Ok(t) => t,
+            Err(_) => {
+                let t = String::from_utf8_lossy(slice);
+                t.to_string()
             }
         }
     }
@@ -189,10 +241,9 @@ impl SshConnection {
     /// // Result to handle
     /// ssh_connection.connect();
     ///
-    /// // Result to handle
-    /// ssh_connection.send(String::from("echo test"));
-    ///
-    /// ssh_connection.disconnect();
+    /// if let Err(e) = ssh_connection.disconnect() {
+    ///     println!("Error : {0}", e);
+    /// }
     /// // Be care, the instance is consume next this called
     /// ```
     pub fn disconnect(self) -> Result<(), SshError> {
